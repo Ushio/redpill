757d9e85073033909cd8a8eb32a6e9a1fed5b2db
ushiostarfish <ushiostarfish@gmail.com>
2022-08-25 00:47:09 +0900

    adjust rendertime

--- nerf.cpp
+++ nerf.cpp
@@ -369,7 +369,7 @@ int main()
 
 		// for( int k = 0; k < 2048; ++k )
 		// 60.0 * 7.0
-		for( int k = 0; executionSW.elapsed() < 60.0 * 5.0; ++k )
+		for( int k = 0; executionSW.elapsed() < 320.0f; ++k )
 		{
 			if( ( k % 16 ) == 0 )
 			{
